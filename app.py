fe19a206f89ef81e5690e8a60bc439daa0e54913
TGNMISYCN-Q <105686540+TGNMISYCN-Q@users.noreply.github.com>
2023-08-07 18:31:34 +0000

    added jasonify

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import Flask, render_template, request, jsonify
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def FBD():
+    return render_template('home.html')
+
+@app.route('/api/jobs')
+def question():
+  return jsonify(question)   #question is going to be a list which contain the question that the users enter
 
 if __name__=='__main__':
   app.run(host='0.0.0.0',debug=True)
+
+@app.route('/questionstore')
+def questionstore():
+  return render_template('QUESTION.html', Questions=Questions)
+           
+Questions=[]
+@app.route('/questionupdate', methods=['POST'])
+def Question():
+  Question=request.form.get('question')
+  if not question:
+    return render_template('home.html')
+  Questions.append(f'{Question}')
+  return redirect('/questionstore')
+
+
+
+
